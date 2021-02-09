12f9b55396e2d47de9060e002c991122acbf72c9
Johannes Flotzinger <69507343+jfltzngr@users.noreply.github.com>
2021-02-09 08:16:47 +0100

    Update namechanger.py

--- namechanger.py
+++ namechanger.py
@@ -1,14 +1,14 @@
 import os 
    
 def namechanger(folder='01_xyz', pref_from_front=True, len_pref=2, len_suf=7):
-	"""change the name of files in a folder according to the folders name.
+	"""change the name of files in a folder according to the folder's name.
         Args:
-		folder: string relative path of the folder
+		folder: 	 relative path of the folder (str)
 		pref_from_front: Choose whether you want to take the prefix of your filename
-					     from the front or rear of your foldername
-		len_pref: number of characters you want from your foldername for the prefix of
-				  your filename in
-		len_suf: number of characters for the suffix
+			 	 from the front or rear of your foldername (bool_ex)
+		len_pref: 	 number of characters you want from your foldername for the prefix of
+	                  	 your filename in (int)
+		len_suf: 	 number of characters for the suffix (int)
 	"""
 	os.chdir(folder)
 	print(os.getcwd())
@@ -19,7 +19,7 @@ def namechanger(folder='01_xyz', pref_from_front=True, len_pref=2, len_suf=7):
 		if pref_from_front:
 			f_name_pref = folder[:len_pref]
 		else:
-		    f_name_pref = folder[-len_pref:]
+			f_name_pref = folder[-len_pref:]
 		# Define suffix:
 		f_name_suf = '{num:0{len_suf}d}'.format(num=i+1, len_suf=len_suf-1)
 		# Add prefix and suffix separated by '_':
